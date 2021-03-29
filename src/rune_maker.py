9de595b6efdd5136c47b57d408c51ad094ca779f
Javier Valenzuela <javier.valenzuela@utexas.edu>
2021-03-29 17:54:03 -0500

    Add argument support
    Add argument support

--- src/rune_maker.py
+++ src/rune_maker.py
@@ -1,5 +1,6 @@
 import time
 import sys
+import argparse
 from random import randint
 from pynput.keyboard import Key, Controller
 
@@ -9,21 +10,41 @@ keyboard = Controller()
 
 # TODO: validate dictionaries
 VOC_REGEN = {'mage' : 2, 'paladin' : 4/3}
-MANA_PER_RUNE = {'ava' : 530, 'sd': 945, 'f_bomb' : 600, 'e_wall': 1000, 'w_growth' : 600}
+MANA_PER_SPELL = {'ava' : 530, 'sd': 945, 'f_bomb' : 600, 'e_wall': 1000, 'w_growth' : 600, 'spear': 350, 'holy': 300}
 FOOD_TIMERS = {'d_ham' : 720, 'b_mushroom': 264}
 
+parser = argparse.ArgumentParser(description='Rune maker')
+
+parser.add_argument('--spell',
+                    choices=list(MANA_PER_SPELL.keys()),
+                    default='ava',
+                    help='Spell type')
+
+parser.add_argument('--food',
+                    choices=list(FOOD_TIMERS.keys()),
+                    default='d_ham',
+                    help='Food type')
+
+parser.add_argument('--voc',
+                    choices=list(VOC_REGEN.keys()),
+                    default='mage',
+                    help='Vocation')
+
+args = parser.parse_args()
 
 # TODO: argparse variables
 # PARAMETERS
-rune = 'ava'
-food = 'b_mushroom'
-voc  = 'mage'
+spell = args.spell
+food = args.food
+voc  = args.voc
 
+print("Running Automationd for:")
+print(f"{voc} using \'{spell}\' eating {food}")
 
 RUNE_HOTKEY = Key.f2
 FOOD_HOTKEY = Key.f1
 
-RUNE_DURATION = MANA_PER_RUNE[rune] / VOC_REGEN[voc]
+RUNE_DURATION = MANA_PER_SPELL[spell] / VOC_REGEN[voc]
 FOOD_DURATION = FOOD_TIMERS[food]
 
 # Startup time
@@ -35,7 +56,7 @@ def key_event(key):
     keyboard.release(key)
 
 
-runer_struct = [('rune', RUNE_HOTKEY, RUNE_DURATION),('food', FOOD_HOTKEY, FOOD_DURATION)]
+runer_struct = [('spell', RUNE_HOTKEY, RUNE_DURATION),('food', FOOD_HOTKEY, FOOD_DURATION)]
 
 counters = [0] * len(runer_struct)
 
